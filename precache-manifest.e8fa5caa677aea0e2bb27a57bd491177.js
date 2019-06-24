23bf4e0bcd1e8e9ea321d5f7a9834d70d403e992
lukovicmarko <lukovicmarko86@gmail.com>
2019-06-24 23:33:23 +0200

    Updates

---
+++ precache-manifest.e8fa5caa677aea0e2bb27a57bd491177.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "463990120ab1f51f2839",
+    "url": "/shop/static/js/runtime~main.4aa8b2a3.js"
+  },
+  {
+    "revision": "2b5b41033f56e01a01ae",
+    "url": "/shop/static/js/main.3fa73c9c.chunk.js"
+  },
+  {
+    "revision": "cd0f9b0a472c7e786188",
+    "url": "/shop/static/js/2.656340c0.chunk.js"
+  },
+  {
+    "revision": "2b5b41033f56e01a01ae",
+    "url": "/shop/static/css/main.ff06fd29.chunk.css"
+  },
+  {
+    "revision": "cd0f9b0a472c7e786188",
+    "url": "/shop/static/css/2.5ac5e7f6.chunk.css"
+  },
+  {
+    "revision": "a7d0a46e8b7be29fa72990cfdb876461",
+    "url": "/shop/index.html"
+  }
+];
