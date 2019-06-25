80c1270ca8ddd23c74eedd6b4253bd7797e0710f
lukovicmarko <lukovicmarko86@gmail.com>
2019-06-26 00:07:49 +0200

    Updates

---
+++ precache-manifest.d88cc366a41e7f461fa09ab6ad8ba780.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "463990120ab1f51f2839",
+    "url": "/shop/static/js/runtime~main.4aa8b2a3.js"
+  },
+  {
+    "revision": "0efb311be00ec82f81b2",
+    "url": "/shop/static/js/main.4bcbacea.chunk.js"
+  },
+  {
+    "revision": "b1905af18ac62245522c",
+    "url": "/shop/static/js/2.1eda7999.chunk.js"
+  },
+  {
+    "revision": "0efb311be00ec82f81b2",
+    "url": "/shop/static/css/main.26424454.chunk.css"
+  },
+  {
+    "revision": "b1905af18ac62245522c",
+    "url": "/shop/static/css/2.5ac5e7f6.chunk.css"
+  },
+  {
+    "revision": "9fbbfc9a0b8d385a405667f2e03f6eea",
+    "url": "/shop/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/shop/precache-manifest.1482f646e66e2e9f2222be4581998614.js"
+  "/shop/precache-manifest.d88cc366a41e7f461fa09ab6ad8ba780.js"
 );
 
 workbox.clientsClaim();
